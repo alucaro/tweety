7b6e3cd54151c34782b24a66f246224ebdf49af1
Adriana Campo <adrianac@talentu.co>
2020-04-29 23:01:58 -0500

    Design the timeline

---
+++ resources/views/_tweet.blade.php
@@ -0,0 +1,19 @@
+<div class="flex p-4 border-b border-b-gray-400">
+    <div class="mr-2 flex-shrink-0">
+        <img 
+            src="https://i.pravatar.cc/50" 
+            alt=""
+            class="rounded-full mr-2"
+        >
+    </div>
+
+    <div>
+        <h5 class="font-bold mb-4">Jhon Doe</h5>
+
+        <p class="text-sm">
+            Writing is often a neglected skill in the classroom and when it does take place it almost always seems to focus on long texts such as letters and essays. This is extremely unfortunate for a number of reasons.
+        </p>
+
+    </div>
+
+</div>
