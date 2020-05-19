0fde4c7836a81fd6a3266aacf9c75da71fd44872
Adriana Campo <adrianac@talentu.co>
2020-05-19 17:56:40 -0500

    Build the follow form

--- resources/views/_tweet.blade.php
+++ resources/views/_tweet.blade.php
@@ -1,6 +1,6 @@
-<div class="flex p-4 border-b border-b-gray-400">
+<div class="flex p-4 {{ $loop->last ? '' : 'border-b border-b-gray-400' }}">
     <div class="mr-2 flex-shrink-0">
-        <a href="{{ route('profile', $tweet->user) }}">
+        <a href="{{ $tweet->user->path() }}">
             <img 
             src="{{ auth()->user()->avatar }}" 
             alt=""
@@ -12,12 +12,14 @@
     </div>
 
     <div>
-        <h5 class="font-bold mb-4">{{ $tweet->user->name }}</h5>
+        <h5 class="font-bold mb-2">
+            <a href="{{ $tweet->user->path() }}">
+                {{ $tweet->user->name }}
+            </a>
+        </h5>
 
         <p class="text-sm">
-            <a href="{{ route('profile', $tweet->user) }}">
                 {{ $tweet->body }}
-            </a>
         </p>
 
     </div>
--- resources/views/profiles/show.blade.php
+++ resources/views/profiles/show.blade.php
@@ -1,37 +1,49 @@
 <x-app>
   <header class="mb-6 relative">
-    <img 
-      src="/images/default-profile-banner.jpg" 
-      alt=""
-      class="mb-2"
-    >
+    <div class="relative">
 
-    <div class="flex justify-between items-center">
+      <img 
+        src="/images/default-profile-banner.jpg" 
+        alt=""
+        class="mb-2"
+      >
+
+      <img 
+        src="{{ $user->avatar }}" 
+        alt=""
+        class="rounded-full mr-2 absolute transform -translate-x-1/2 translate-y-1/2"
+        style="left: 50%;bottom: 0;"
+        width= "150"
+      >
+
+    </div>
+
+    <div class="flex justify-between items-center mb-0">
       <div>
         <h2 class="font-bold text-2xl mb-0"> {{ $user->name }}</h2>
         <p class="text-sm">Joined {{ $user->created_at->diffForHumans() }}</p>
       </div>
 
-      <div>
-        <a href="" class="rounded-full border border-gray-300 py-2 px-2 text-black text-xs mr-2">Edit profie</a>
-        <a href="" class="bg-blue-500 rounded-full shadow py-2 px-4 text-white text-xs">Follow me</a>
+      <div class="flex">
+        <a 
+          href="" 
+          class="rounded-full border border-gray-300 py-2 px-2 text-black text-xs mr-2"
+          >
+            Edit profie
+          </a>
+
+          <x-follow-button :user="$user"></x-follow-button>
+
       </div>
+
     </div>
 
     
-    <p class="text-sm">
+    <p class="text-sm mt-4">
       Tom and Jerry is an American animated franchise and series of comedy short films created in 1940 by William Hanna and Joseph Barbera. 
       Best known for its 161 theatrical short films by Metro-Goldwyn-Mayer, the series centers on a friendship/rivalry (a love-hate relationship) 
       between the title characters Tom, a cat, and Jerry, a mouse. Many shorts also feature several recurring characters.
     </p>
-
-    <img 
-      src="{{ $user->avatar }}" 
-      alt=""
-      class="rounded-full mr-2 absolute"
-      style="width: 150px; left: calc(50% - 75px); top:55%"
-    >
-
 
   </header>
 
